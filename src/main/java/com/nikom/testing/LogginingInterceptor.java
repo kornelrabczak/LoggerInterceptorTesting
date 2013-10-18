acc2dda2057b1f7d56ae9cc0cce8e476f8a34136
nikom <nikom1337@gmail.com>
2013-10-18 17:11:12 +0200

    init

---
+++ src/main/java/com/nikom/testing/LogginingInterceptor.java
@@ -0,0 +1,14 @@
+package com.nikom.testing;
+
+import javax.interceptor.InvocationContext;
+
+/**
+ * User: nikom
+ */
+public class LogginingInterceptor {
+
+    public Object invoke(InvocationContext ctx) throws Exception {
+        return null;
+    }
+
+}
---
+++ src/main/java/com/nikom/testing/SomeRandomClazz.java
@@ -0,0 +1,15 @@
+package com.nikom.testing;
+
+import javax.interceptor.Interceptors;
+
+/**
+ * User: nikom
+ */
+public class SomeRandomClazz {
+
+    @Interceptors(LogginingInterceptor.class)
+    public boolean someRandomBooleanMethod() {
+        return true;
+    }
+
+}
